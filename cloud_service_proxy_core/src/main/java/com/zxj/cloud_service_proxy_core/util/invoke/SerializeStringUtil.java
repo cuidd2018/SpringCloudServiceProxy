7b5a3bedef81844dee3bd013d2ec10d4f86b2078
zhuxiujia <zhuxiujia@qq.com>
2018-01-03 16:13:29 +0800

    fix

--- cloud_service_proxy_core/src/main/java/com/zxj/cloud_service_proxy_core/util/invoke/SerializeStringUtil.java
+++ cloud_service_proxy_core/src/main/java/com/zxj/cloud_service_proxy_core/util/invoke/SerializeStringUtil.java
@@ -13,28 +13,47 @@ public class SerializeStringUtil {
 
     public static byte[] serialize(Object obj) throws IOException {
         if(obj==null) throw new NullPointerException();
-
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        HessianOutput ho = new HessianOutput(os);
-        ho.writeObject(obj);
-        return os.toByteArray();
+        HessianOutput hessianOutput = new HessianOutput(os);
+        hessianOutput.writeObject(obj);
+        byte[] byteArray= os.toByteArray();
+        try{
+            if(hessianOutput!=null)hessianOutput.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try{
+            if(os!=null)os.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return byteArray;
     }
 
     public static Object deserialize(byte[] by) throws IOException{
         if(by==null) throw new NullPointerException();
         ByteArrayInputStream is = new ByteArrayInputStream(by);
         HessianInput hi = new HessianInput(is);
-        return hi.readObject();
+        Object object= hi.readObject();
+        try {
+            if(hi!=null)hi.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try{
+            if(is!=null)is.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return object;
     }
 
 
     public static String serializeToByteString(Object obj)  {
         if(obj==null) return null;
         try {
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            HessianOutput ho = new HessianOutput(os);
-            ho.writeObject(obj);
-            String  result = new String(os.toByteArray(),CHARTSET_FILE);
+            byte[] bytes= serialize(obj);
+            String  result = new String(bytes,CHARTSET_FILE);
             return result;
         }catch (Exception e){
             throw new RuntimeException(e);
