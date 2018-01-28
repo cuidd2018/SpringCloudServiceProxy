ccc362b2b3c57bba7a491a737d7f1edeb5734c04
ce <zhuxiujia@qq.com>
2018-01-28 21:32:03 +0800

    优化

--- cloud_service_proxy_core/src/main/java/com/zxj/cloud_service_proxy_core/util/invoke/RemoteServiceProxyFactory.java
+++ cloud_service_proxy_core/src/main/java/com/zxj/cloud_service_proxy_core/util/invoke/RemoteServiceProxyFactory.java
@@ -51,21 +51,21 @@ public class RemoteServiceProxyFactory implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         String methodName = method.getName();
         Class[] paramsTypes = method.getParameterTypes();
-        String _service = (String) BeanUtils.getProperty(BeanUtils.getProperty(method, "clazz"), "name");
+        String serviceName = (String) BeanUtils.getProperty(BeanUtils.getProperty(method, "clazz"), "name");
         ServiceDTO serviceDTO = new ServiceDTO();
         serviceDTO.setMethod(methodName);
-        serviceDTO.setService(_service);
+        serviceDTO.setService(serviceName);
         serviceDTO.setParams(args);
         serviceDTO.setParamsTypes(paramsTypes);
         byte[] bytes = SerializeStringUtil.serialize(serviceDTO);
         if (restTempleteProvider == null)
             throw new ServiceException("restTempleteProvider can not be null!");
         if (restTempleteProvider.getRestTemplete() == null)
-            throw new ServiceException("can not restTempleteProvider.getRestTemplete()=" + _service);
+            throw new ServiceException("can not restTempleteProvider.getRestTemplete()=" + serviceName);
         try {
             logger.info("bytesLength:" + bytes.length);
             HttpEntity<byte[]> httpEntity = new HttpEntity<>(bytes, httpHeaders);
-            String remoteUrl="http://" + restTempleteProvider.service() + "/" + getRemoteServiceMethod(_service);
+            String remoteUrl="http://" + restTempleteProvider.service() + "/" + getRemoteServiceMethod(serviceName);
             ResponseEntity<? extends byte[]> response = ((RestTemplate) restTempleteProvider.getRestTemplete())
                     .exchange(remoteUrl, HttpMethod.PUT, httpEntity, byteArrayClassType);
             byte[] bytesResult = response.getBody();
@@ -83,7 +83,7 @@ public class RemoteServiceProxyFactory implements InvocationHandler {
             }
             return result;
         } catch (Exception e) {
-            logger.error("远程代理服务" + _service + "调用失败", e);
+            logger.error("远程代理服务" + serviceName + "调用失败", e);
             throw e;
         }
     }
