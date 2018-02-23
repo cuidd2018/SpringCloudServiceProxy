35ce948b3c7da7a9c16528ca3701294d4315f93b
zhuxiujia <zhuxiujia@qq.com>
2018-02-23 10:03:10 +0800

    修改相关Exception类

--- cloud_service_proxy_core/src/main/java/com/zxj/cloud_service_proxy_core/exception/ServiceException.java
+++ cloud_service_proxy_core/src/main/java/com/zxj/cloud_service_proxy_core/exception/ServiceException.java
@@ -1,75 +1,113 @@
 package com.zxj.cloud_service_proxy_core.exception;
 
-
 import com.zxj.cloud_service_proxy_core.enums.ServiceProxyErrorCode;
 import com.zxj.cloud_service_proxy_core.variable.IntEnumVariable;
 
 /**
  * 普通业务异常
- * 
+ *
  * @author zhuxiujie
  * @since 2016年8月11日 上午10:21:19
  */
-public class ServiceException extends Exception implements BaseExceptionInterface{
+public class ServiceException extends Exception implements BaseExceptionInterface {
 
-	private static final long serialVersionUID = -6166718557157998012L;
+    private static final long serialVersionUID = -6166718557157998012L;
 
-	private Integer				errCode;
+    private Integer errCode;
 
-	private String				errMsg;
+    private String errMsg;
 
+    public ServiceException(IntEnumVariable serviceErrorCode) {
+        this(serviceErrorCode.getValue(), serviceErrorCode.getName());
+    }
 
-	public ServiceException(IntEnumVariable intEnumVariable){
-		this(intEnumVariable.getValue(),intEnumVariable.getName());
-	}
-	public ServiceException(IntEnumVariable serviceErrorCode, String errMsg){
-		this(serviceErrorCode.getValue(),serviceErrorCode.getName()+errMsg);
-	}
+    public ServiceException(IntEnumVariable serviceErrorCode, String errMsg) {
+        this(serviceErrorCode.getValue(), serviceErrorCode.getName() + errMsg);
+    }
 
-	public ServiceException(String info) {
-		this(ServiceProxyErrorCode.ERROR.getValue(), info);
-	}
+    public ServiceException(BaseExceptionInterface e) {
+        this(e.getErrCode(), e.getErrMsg());
+    }
 
-	public ServiceException(Object... info) {
-		this(ServiceProxyErrorCode.ERROR.getValue(), getInfos(info));
-	}
+    public ServiceException(String info) {
+        this(ServiceProxyErrorCode.ERROR.getValue(), info);
+    }
 
-	public ServiceException(IntEnumVariable intEnumVariable, Object... info) {
-		this(intEnumVariable, getInfos(info));
-	}
+    public ServiceException(Object... infos) {
+        this(ServiceProxyErrorCode.ERROR, getInfos(infos));
+    }
 
-	private static String getInfos(Object[] info) {
-		if(info!=null&&info.length!=0) {
-			StringBuilder stringBuilder = new StringBuilder();
-            for (Object _info:info){
-            	stringBuilder.append(_info);
-			}
-			return stringBuilder.toString();
-		}
-		return "错误";
-	}
-
-	public ServiceException(Integer errCode, String errMsg) {
-		super(String.format("errCode: %d, errMsg: %s", errCode, errMsg));
-		this.setErrCode(errCode);
-		this.setErrMsg(errMsg);
-	}
-	@Override
-	public Integer getErrCode() {
-		return errCode;
-	}
-	@Override
-	public String getErrMsg() {
-		return errMsg;
-	}
-
-	public void setErrCode(Integer errCode) {
-		this.errCode = errCode;
-	}
-
-	public void setErrMsg(String errMsg) {
-		this.errMsg = errMsg;
-	}
+    public ServiceException(IntEnumVariable serviceErrorCode, Object... info) {
+        this(serviceErrorCode, getInfos(info));
+    }
 
 
+    public ServiceException(Exception e) {
+        this(getErrorCode(e), getExceptionInfo(e));
+    }
+
+    public ServiceException(Exception e, String extraInfo) {
+        this(getErrorCode(e), extraInfo + getExceptionInfo(e));
+    }
+
+    public ServiceException(String extraInfo, Exception e) {
+        this(getErrorCode(e), extraInfo + getExceptionInfo(e));
+    }
+
+    private static String getExceptionInfo(Exception e) {
+        if (e != null && e instanceof BaseExceptionInterface) {
+            return ((BaseExceptionInterface) e).getErrMsg();
+        } else {
+            return e.toString();
+        }
+    }
+
+    private static Integer getErrorCode(Exception e) {
+        if (e != null && e instanceof BaseExceptionInterface) {
+            return ((BaseExceptionInterface) e).getErrCode();
+        } else {
+            return ServiceProxyErrorCode.ERROR.getValue();
+        }
+    }
+
+
+    private static String getInfos(Object[] info) {
+        if (info != null && info.length != 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Object object : info) {
+                if (object instanceof Exception) {
+                    stringBuilder.append(getExceptionInfo((Exception) object));
+                } else {
+                    stringBuilder.append(object);
+                }
+            }
+            return stringBuilder.toString();
+        }
+        return "错误";
+    }
+
+
+    public ServiceException(Integer errCode, String errMsg) {
+        super(String.format("errCode: %d, errMsg: %s", errCode, errMsg));
+        this.setErrCode(errCode);
+        this.setErrMsg(errMsg);
+    }
+
+    @Override
+    public Integer getErrCode() {
+        return errCode;
+    }
+
+    @Override
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public void setErrCode(Integer errCode) {
+        this.errCode = errCode;
+    }
+
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+    }
 }
--- cloud_service_proxy_core/src/main/java/com/zxj/cloud_service_proxy_core/exception/ServiceRuntimeException.java
+++ cloud_service_proxy_core/src/main/java/com/zxj/cloud_service_proxy_core/exception/ServiceRuntimeException.java
@@ -1,6 +1,5 @@
 package com.zxj.cloud_service_proxy_core.exception;
 
-
 import com.zxj.cloud_service_proxy_core.enums.ServiceProxyErrorCode;
 import com.zxj.cloud_service_proxy_core.variable.IntEnumVariable;
 
@@ -8,26 +7,34 @@ import com.zxj.cloud_service_proxy_core.variable.IntEnumVariable;
  * 运行时异常，请注意抛出位置，会触发事务回滚！！！！！！！！！
  * Created by ce on 2017/4/14.
  */
-public class ServiceRuntimeException extends RuntimeException implements BaseExceptionInterface{
+public class ServiceRuntimeException extends RuntimeException implements BaseExceptionInterface {
 
 
     private static final long serialVersionUID = -6509426443798876274L;
 
-    private Integer				errCode;
+    private Integer errCode;
 
-    private String				errMsg;
+    private String errMsg;
 
-    public ServiceRuntimeException(IntEnumVariable serviceErrorCode){
-        this(serviceErrorCode.getValue(),serviceErrorCode.getName());
+    public ServiceRuntimeException(IntEnumVariable serviceErrorCode) {
+        this(serviceErrorCode.getValue(), serviceErrorCode.getName());
     }
 
-    public ServiceRuntimeException(IntEnumVariable serviceErrorCode, String errMsg){
-        this(serviceErrorCode.getValue(),serviceErrorCode.getName()+errMsg);
+    public ServiceRuntimeException(IntEnumVariable serviceErrorCode, String errMsg) {
+        this(serviceErrorCode.getValue(), serviceErrorCode.getName() + errMsg);
     }
 
     public ServiceRuntimeException(String errMsg) {
         this(ServiceProxyErrorCode.ERROR.getValue(), errMsg);
         this.setErrCode(ServiceProxyErrorCode.ERROR.getValue());
+    }
+
+    public ServiceRuntimeException(BaseExceptionInterface e) {
+        this(e.getErrCode(), e.getErrMsg());
+    }
+
+    public ServiceRuntimeException(ServiceException e) {
+        this(e.getErrCode(), e.getErrMsg());
     }
 
     public ServiceRuntimeException(Integer errCode, String errMsg) {
@@ -40,26 +47,58 @@ public class ServiceRuntimeException extends RuntimeException implements BaseExc
         this(ServiceProxyErrorCode.ERROR.getValue(), getInfos(info));
     }
 
+    public ServiceRuntimeException(Exception e) {
+        this(getErrorCode(e), getExceptionInfo(e));
+    }
+
+    public ServiceRuntimeException(Exception e, String extraInfo) {
+        this(getErrorCode(e), extraInfo + getExceptionInfo(e));
+    }
+
+    public ServiceRuntimeException(String extraInfo, Exception e) {
+        this(getErrorCode(e), extraInfo + getExceptionInfo(e));
+    }
+
+    private static String getExceptionInfo(Exception e) {
+        if (e != null && e instanceof BaseExceptionInterface) {
+            return ((BaseExceptionInterface) e).getErrMsg();
+        } else {
+            return e.toString();
+        }
+    }
+
+    private static Integer getErrorCode(Exception e) {
+        if (e != null && e instanceof BaseExceptionInterface) {
+            return ((BaseExceptionInterface) e).getErrCode();
+        } else {
+            return ServiceProxyErrorCode.ERROR.getValue();
+        }
+    }
+
     public ServiceRuntimeException(IntEnumVariable serviceErrorCode, Object... info) {
         this(serviceErrorCode, getInfos(info));
     }
 
     private static String getInfos(Object[] info) {
-        if(info!=null&&info.length!=0) {
+        if (info != null && info.length != 0) {
             StringBuilder stringBuilder = new StringBuilder();
-            for (Object _info:info){
-                stringBuilder.append(_info);
+            for (Object object : info) {
+                if (object instanceof Exception) {
+                    stringBuilder.append(getExceptionInfo((Exception) object));
+                } else {
+                    stringBuilder.append(object);
+                }
             }
             return stringBuilder.toString();
         }
         return "错误";
     }
 
-
     @Override
     public Integer getErrCode() {
         return errCode;
     }
+
     @Override
     public String getErrMsg() {
         return errMsg;
@@ -72,4 +111,5 @@ public class ServiceRuntimeException extends RuntimeException implements BaseExc
     public void setErrMsg(String errMsg) {
         this.errMsg = errMsg;
     }
+
 }
