8172899b1a54ebbfb998949160953784b83938be
zhuxiujia <zhuxiujia@qq.com>
2018-07-03 17:16:47 +0800

    升级框架兼容SpringBoot 2.0,Spring 5.0,Spring Cloud Finchley.版本皆为GA版本

--- demo_discovery/src/main/java/com/example/demo_discovery/DemoDiscoveryApplication.java
+++ demo_discovery/src/main/java/com/example/demo_discovery/DemoDiscoveryApplication.java
@@ -1,11 +1,16 @@
 package com.example.demo_discovery;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAutoConfiguration
 public class DemoDiscoveryApplication {
 
 	public static void main(String[] args) {
