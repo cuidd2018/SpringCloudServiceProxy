b8057125612ef5fd282604f22defe331bbeb66d0
zhuxiujia <zhuxiujia@qq.com>
2018-07-16 16:38:33 +0800

    兼容jdk10

--- demo_service/src/main/java/com/example/demo/DemoServiceApplication.java
+++ demo_service/src/main/java/com/example/demo/DemoServiceApplication.java
@@ -1,9 +1,5 @@
 package com.example.demo;
 
-import com.alibaba.fastjson.JSON;
-import com.example.demo_service_interface.page.Page;
-import com.example.demo_service_interface.page.PageRequest;
-import com.zxj.cloud_service_proxy_core.exception.ServiceException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -12,15 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class DemoServiceApplication {
 
-	public static void main(String[] args) throws ServiceException {
-		//SpringApplication.run(DemoServiceApplication.class, args);
-
-
-		PageRequest page= PageRequest.create(1,20);
-
-		var str=JSON.toJSONString(page);
-
-		PageRequest request=JSON.parseObject(str,PageRequest.class);
-		System.out.println(request);
+	public static void main(String[] args)  {
+		SpringApplication.run(DemoServiceApplication.class, args);
 	}
 }
